244b120305c133a298b0c2ad3e6bb6f8ff66d608
Pedro Soares <ngrpedro22@gmail.com>
2023-02-20 08:47:31 -0300

    fix:: RadixAvatar fallback

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,20 +1,37 @@
-import React, { ImgHTMLAttributes } from "react";
+import React, { ImgHTMLAttributes } from 'react'
+import * as RadixAvatar from '@radix-ui/react-avatar'
+import { User } from 'phosphor-react'
 
 interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
-  hasBorder?: boolean;
+  hasBorder?: boolean
 }
 
 const Avatar = ({ hasBorder = true, ...props }: AvatarProps) => {
   return (
-    <div
+    <>
+      <RadixAvatar.Root
+        className={`w-14 h-14 rounded-full border-[px] overflow-hidden 
+              bg-gray-500 flex items-center justify-center ${
+                hasBorder &&
+                'outline-none outline-green-700 outline-[3px] border-zinc-900'
+              }`}
+      >
+        <RadixAvatar.Image {...props} className='w-full h-full' />
+        <RadixAvatar.Fallback className='' delayMs={600}>
+          <User size={32} />
+        </RadixAvatar.Fallback>
+      </RadixAvatar.Root>
+
+      {/* <div
       className={`w-14 h-14 rounded-full border-[px] overflow-hidden ${
         hasBorder &&
         "outline-none outline-green-700 outline-[3px] border-zinc-900"
       }`}
-    >
-      <img {...props} className="w-full h-full" />
-    </div>
-  );
-};
+      >
+     
+    </div> */}
+    </>
+  )
+}
 
-export default Avatar;
+export default Avatar
